a7cdb1533a285db9b26d9e90ca36f8a5d36622df
Houndzkie <helge.justine.dano@gmail.com>
2025-06-05 17:14:41 +0800

    singly linked list

---
+++ Linked List/singly-linked-list.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+// Node definition
+struct Node {
+  int data;
+  Node* next;
+
+  Node(int val) : data(val), next(nullptr) {}
+};
+
+// Linked list class
+class LinkedList {
+  private:
+    Node* head;
+
+  public:
+    LinkedList() : head(nullptr) {}
+
+    // Insert at end
+    void insertAtEnd(int val) {
+      Node* newNode = new Node(val);
+
+      if (!head) {
+        head = newNode;
+        return;
+      }
+
+      Node* temp = head;
+
+      while (temp->next) {
+        temp = temp->next;
+      }
+
+      temp->next = newNode;
+    }
+
+    // Insert at the beggining
+    void insertAtBeginning(int val) {
+      Node* newNode = new Node(val);
+      newNode->next = head;
+      head = newNode;
+    }
+
+    // Delete a node by value
+    void deleteValue(int val) {
+      if (!head) return;
+
+      if (head->data == val) {
+        Node* toDelete = head;
+        head = head->next;
+        delete toDelete;
+        return;
+      }
+
+      Node* temp = head;
+
+      while (temp->next && temp->next->data != val) {
+        temp = temp->next;
+      }
+
+      if (temp->next) {
+        Node* toDelete = temp->next;
+        temp->next = temp->next->next;
+        delete toDelete;
+      }
+    }
+
+    // Display list
+    void display() {
+      Node* temp = head;
+
+      while (temp) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+      }
+
+      cout << "NULL\n";
+    }
+
+    // Destructor to free memory
+    ~LinkedList() {
+      Node* curr = head;
+
+      while (curr) {
+        Node* nextNode = curr->next;
+        delete curr;
+        curr = nextNode;
+      }
+    }
+};
+
+// Test the linked list
+int main() {
+    LinkedList list;
+
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtBeginning(5);
+    list.display(); // Output: 5 -> 10 -> 20 -> NULL
+
+    list.deleteValue(10);
+    list.display(); // Output: 5 -> 20 -> NULL
+
+    return 0;
+}
